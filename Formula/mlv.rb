7f32de37f7c61ae57a8c9e8c80d131757d72fa34
Andrii Hlybchenko <hlybchenko.andrii@gmail.com>
2025-08-17 17:41:50 +0300

    new release: 0.2.1-alpha

--- Formula/mlv.rb
+++ Formula/mlv.rb
@@ -1,9 +1,9 @@
 class Mlv < Formula
   desc "Malva CLI for trimming, watermarking, resizing and GIF conversion"
   homepage "https://github.com/Just-Love-You/malva-cli"
-  url "https://github.com/Just-Love-You/malva-cli/archive/refs/tags/v0.2.0-alpha.tar.gz"
-  version "0.2.0-alpha"
-  sha256 "73ff771570fb742cc7e03fd36c0d13aed4f984630fcee94bd569bc03db07a8a2"
+  url "https://github.com/Just-Love-You/malva-cli/archive/refs/tags/v0.2.1-alpha.tar.gz"
+  version "0.2.1-alpha"
+  sha256 "659a7d2a10da710366f8ab6e392f121609b0c22727721b1c80a1ccbffa3714b9"
   license "MIT"
 
   depends_on "go" => :build
